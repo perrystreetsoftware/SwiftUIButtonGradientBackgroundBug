af4d3383b332b27dee65dba83cee1857885c4fc7
Petr Pavlik <petrpavlik@me.com>
2023-08-08 10:58:48 +0200

    bug code

--- SwiftUIBugExample/ContentView.swift
+++ SwiftUIBugExample/ContentView.swift
@@ -7,13 +7,54 @@
 
 import SwiftUI
 
+public extension Color {
+    @inlinable init(hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+}
+
+public struct PrimaryJackdButtonStyle: ButtonStyle {
+
+    private let isLoading: Bool
+
+    public init(isLoading: Bool = false) {
+        self.isLoading = isLoading
+    }
+
+    public func makeBody(configuration: Self.Configuration) -> some View {
+        configuration
+            .label
+            .font(Font.system(size: 16.0).weight(.heavy))
+            .lineLimit(2)
+            .multilineTextAlignment(.center)
+            .padding(13)
+            .opacity(configuration.isPressed ? 0.5 : 1)
+            .frame(minWidth: 175, maxWidth: .infinity)
+            .background(
+                // BUG: tapping on the buttons makes it disappear Xcode 14.3.1
+                // Reproduced on both iOS 16.6 device and Xcode 14.3.1 simulator. Even when running in SwiftUI preview.
+               // wrapping `LinearGradient` in a view, such as Group or ZStack fixes the issue
+                LinearGradient(gradient: Gradient(colors: [Color(hex: 0xDF123A), Color(hex: 0xFC7517)]), startPoint: .leading, endPoint: .trailing)
+                .opacity(configuration.isPressed ? 0.5 : 1)
+            )
+            .cornerRadius(25)
+            .foregroundColor(isLoading ? .clear : .white)
+            .allowsHitTesting(!isLoading)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button("Test") {
+                
+            }.buttonStyle(PrimaryJackdButtonStyle())
         }
         .padding()
     }
