60ab339e1e74bd9e517f0d3a8175cb1690d3a92a
Petr Pavlik <petrpavlik@me.com>
2023-08-08 10:22:08 +0200

    Initial Commit

---
+++ SwiftUIBugExample/SwiftUIBugExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBugExampleApp.swift
+//  SwiftUIBugExample
+//
+//  Created by Petr Pavlik on 08.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBugExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
